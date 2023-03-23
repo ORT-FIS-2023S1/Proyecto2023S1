7a8f5145ffd4851ab8940ce2b98077f0ba32a1a3
Martin Solari <martinsolari@gmail.com>
2023-03-23 11:17:46 -0300

    script principal renombrado main

--- src/main.js
+++ src/main.js
@@ -2,5 +2,5 @@ import { Pais } from "./domain/pais.js";
 const btnGuardar = document.getElementById('btn_guardar');
 btnGuardar.addEventListener('click', () => {
   const nuevoPais = new Pais('Uruguay');
-  nuevoPais.presentar();
+  console.log(nuevoPais.presentar());
 } );
